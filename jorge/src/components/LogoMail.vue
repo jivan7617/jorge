4f0cc6d33bfcb93a51b8d393ea1b5b6d689aff79
jorge <93635606+jivan7617@users.noreply.github.com>
2025-04-01 15:39:08 -0500

    adding a link to Pages

--- jorge/src/components/LogoMail.vue
+++ jorge/src/components/LogoMail.vue
@@ -12,7 +12,7 @@
         <div class="div_2">
             <ul>
                 <li class="list_2"><a href="mailto:jivan7617@gmail.com">jivan7617@gmail.com</a></li>
-                <li class="list_2"><a href="https://github.com/jivan7617/financiera1"> GitHub </a></li>
+                <li class="list_2"><a href="https://github.com/jivan7617/financiera1"> GitHub </a> <a href="https://jivan7617.github.io/jorge/"> GitPages </a></li>
                 <li class="list_2"><a href="https://www.linkedin.com/in/jorge-iván-espinosa-350089195/?locale=en_US&trk=eml-email_job_alert_confirmation_01-header-25-profile">Linkedin</a></li>
             </ul>
         </div>
